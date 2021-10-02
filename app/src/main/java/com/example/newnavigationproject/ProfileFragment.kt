078518fa0d9309d2951e30753327d1def902379f
okezino <okezi002@gmail.com>
2021-10-02 16:36:30 +0100

    deep Linking

--- app/src/main/java/com/example/newnavigationproject/ProfileFragment.kt
+++ app/src/main/java/com/example/newnavigationproject/ProfileFragment.kt
@@ -6,15 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.google.android.material.snackbar.Snackbar
 
 
 class ProfileFragment : Fragment() {
 
+
+
     lateinit var textViewButton : TextView
     lateinit var mainViewModel : MainViewModel
     lateinit var sharedTextView: TextView
+    private val  args : ProfileFragmentArgs by navArgs()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +40,11 @@ class ProfileFragment : Fragment() {
 
         textViewButton = view.findViewById(R.id.profileFragmentTextview)
         sharedTextView = view.findViewById(R.id.profileFragmentSharedValue)
+        var firstDeep = args.rounds
+
+        firstDeep?.let {
+            Snackbar.make(view, "Value from link : $it", Snackbar.LENGTH_LONG).show()
+        }
 
         var repository = Repository("first")
         var ProfileFactory = MainViewModelFactory(repository)
